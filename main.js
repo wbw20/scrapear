773459538401e4ad20a09e91da63e268ee98743f
William B. Wettersten <wbw20@case.edu>
2014-06-03 14:21:47 -0700

    setup async structure

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ var request = require('request'),
     async = require('async'),
     format = require('util').format;
 
-var reddits = [ 'programming', 'javascript', 'node' ]
-  , concurrency = 2;
+var start = 'http://www.foodmaster.com/prodBrowse?code=296',
+    concurrency = 2;
 
-async.eachLimit(reddits, concurrency, function (reddit, next) {
-    var url = format('http://reddit.com/r/%s', reddit);
-    request(url, function (err, response, body) {
-        if (err) throw err;
-        var $ = cheerio.load(body);
-        $('a.title').each(function () {
-            console.log('%s (%s)', $(this).text(), $(this).attr('href'));
-        });
-        next();
-    });
+request(start, function (err, response, body) {
+  if (err) throw err;
+  var $ = cheerio.load(body);
+  async.eachLimit($('a'), concurrency, function (element, next) {
+
+
+    console.log('%s (%s)', $(element).text(), $(element).attr('href'));
+
+    next();
+  });
 });
