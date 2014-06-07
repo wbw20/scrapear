dd9c5caf88ada97b30313f595d1765bc1366cfd7
William B. Wettersten <wbw20@case.edu>
2014-06-07 11:06:53 -0700

    make it publishable

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ var request = require('request'),
     async = require('async'),
     format = require('util').format;
 
-var start = 'http://www.foodmaster.com/prodBrowse?code=296',
-    concurrency = 2,
+var concurrency = 2,
     counter = 0,
     map = {};
 
@@ -12,42 +11,32 @@ function done() {
   console.log(map);
 }
 
-function expand(url, select, cb) {
-  counter++;
+module.exports = {
+  map: function() {
+    return map;
+  },
+  expand: function(url, select, cb) {
+    counter++;
 
-  request(url, function (err, response, body) {
-    if (err) {
-      console.err('X');
-      return
-    }
+    request(url, function (err, response, body) {
+      if (err) {
+        console.err('X');
+        return
+      }
 
-    var $ = cheerio.load(body);
+      var $ = cheerio.load(body);
 
-    async.eachLimit($(select), concurrency, function (element, next) {
-      cb($(element));
-      next();
-    });
+      async.eachLimit($(select), concurrency, function (element, next) {
+        cb($(element));
+        next();
+      });
 
-    counter--;
+      counter--;
 
-    /* finished spidering */
-    if (counter === 0) {
-      done();
-    }
-  });
-}
-
-process.stdout.write('Spidering');
-
-expand(start, 'a', function(element) {
-  process.stdout.write('.');
-  var link = element.attr('href');
-  map[element.text()] = [];
-
-  if (link.indexOf('companies') != -1) {
-    expand(link, 'ul.DIRECTORIES-CHILDPRODUCTS > li > a', function(el) {
-      process.stdout.write('.');
-      map[element.text()].push(el.text());
+      /* finished spidering */
+      if (counter === 0) {
+        done();
+      }
     });
   }
-});
+};
